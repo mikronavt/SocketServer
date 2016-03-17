116f8db54ed5fbca882941ded16ed9590dcea282
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-17 12:38:45 +0300

    main

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+/**
+ * Created by User on 17.03.2016.
+ */
+public class Main {
+    public static void main(String[] args) throws Exception{
+        System.out.println("Server started!");
+        while(true){
+            Thread.sleep(1000);
+        }
+    }
+}
